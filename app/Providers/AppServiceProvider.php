07f806749078c41b6038d4248d0a3033d3218a39
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-09-13 09:19:21 +0300

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Livewire\Vendor\Product\AddProductComponent;
 use App\Livewire\Vendor\Product\Step\ProductDetails;
 use App\Livewire\Vendor\Product\Step\ProductMedia;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
@@ -15,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if (config('app.env') === 'production') {
+            Livewire::setUpdateRoute(function ($handle) {
+                return Route::post('/ras/livewire/update', $handle);
+            });
+        }
         Livewire::component('add-product', AddProductComponent::class);
         Livewire::component('product-details', ProductDetails::class);
         Livewire::component('product-media', ProductMedia::class);
