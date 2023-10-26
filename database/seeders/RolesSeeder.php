4a8f62dde6963b3d6c877ae3ded3303da5e54b2c
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-10-26 09:37:34 +0300

    Update

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -15,47 +15,85 @@ class RolesSeeder extends Seeder
     public function run(): void
     {
         $all_permissions = [
-            'create cart',
-            'view cart',
-            'update cart',
-            'delete cart',
-            'create order',
+            // 'create cart',
+            // 'view cart',
+            // 'update cart',
+            // 'delete cart',
+            // 'create order',
             'view order',
-            'delete order',
+            // 'delete order',
             'update order',
-            'create product',
+            // 'create product',
             'view product',
-            'update product',
-            'delete product',
+            // 'update product',
+            // 'delete product',
+
+            // Users
+            'create user',
+            'update user',
+            'delete user',
+            'view user',
+
+            // Roles
+            'create role',
+            'update role',
+            'delete role',
+
+            // Warehouse
             'create warehouse',
             'view warehouse',
             'update warehouse',
             'delete warehouse',
+
+            // Financier
             'create financier',
             'view finaciers',
             'update finacier',
             'delete finacier',
+
+            // Inspector
+            'create inspector',
+            'view inspector',
+            'update inspector',
+            'delete inspector',
+
+            // Inspection
+            'view inspection report',
+            'create inspection report',
+            'update inspection report',
+            'delete inspection report',
+
+            // Financing
+            'view financing request',
+            'update financing request',
+
+            // Settings
+            'view settings',
+            'update settings',
+
+            // Logs
+            'view logs',
         ];
 
-        $buyer_permissions = [
-            'create cart',
-            'view cart',
-            'update cart',
-            'delete cart',
-            'create order',
-            'delete order',
-            'update order',
-            'view product',
-            'view warehouse',
-            'view finaciers',
-        ];
+        // $buyer_permissions = [
+        //     'create cart',
+        //     'view cart',
+        //     'update cart',
+        //     'delete cart',
+        //     'create order',
+        //     'delete order',
+        //     'update order',
+        //     'view product',
+        //     'view warehouse',
+        //     'view finaciers',
+        // ];
 
-        $vendor_permissions = [
-            'create product',
-            'update product',
-            'delete product',
-            'view warehouse',
-        ];
+        // $vendor_permissions = [
+        //     'create product',
+        //     'update product',
+        //     'delete product',
+        //     'view warehouse',
+        // ];
 
         $warehouse_permissions = [
             'view product',
@@ -65,49 +103,74 @@ class RolesSeeder extends Seeder
         ];
 
         $transaction_permissions = [
-            'create transaction',
-            'view transaction',
-            'update transaction',
-            'delete transaction'
+            'view financing request',
+            'update financing request',
         ];
 
         $inspector_permissions = [
             'view product',
             'view warehouse',
             'view order',
-            'edit order',
+            'update order',
+            'view inspection report',
+            'create inspection report',
+            'update inspection report',
+            'delete inspection report',
         ];
 
-        $roles = ['admin', 'buyer', 'vendor', 'warehouse manager', 'financier', 'inspector'];
+        $deveint_permissions = [
+            'create user',
+            'update user',
+            'delete user',
+            'view user',
+            'create role',
+            'update role',
+            'delete role',
+            'create warehouse',
+            'view warehouse',
+            'update warehouse',
+            'delete warehouse',
+            'view settings',
+            'update settings',
+            'view logs'
+        ];
+
+        $roles = ['admin', 'buyer', 'vendor', 'warehouse manager', 'financier', 'inspector', 'deveint'];
 
         collect($all_permissions)->each(fn ($permission) => Permission::firstOrCreate(['name' => $permission]));
 
-        collect($roles)->each(function ($role) use ($warehouse_permissions, $vendor_permissions, $buyer_permissions, $transaction_permissions, $inspector_permissions){
+        collect($roles)->each(function ($role) use ($warehouse_permissions, $transaction_permissions, $inspector_permissions, $deveint_permissions){
             $role = Role::firstOrCreate(['name' => $role]);
-            if ($role->name === 'vendor') {
-                collect($vendor_permissions)->each(function ($permission) use ($role) {
-                    $role->givePermissionTo($permission);
-                });
-            }
-            if ($role->name === 'buyer') {
-                collect($buyer_permissions)->each(function ($permission) use ($role) {
-                    $role->givePermissionTo($permission);
-                });
-            }
+            // if ($role->name === 'vendor') {
+            //     collect($vendor_permissions)->each(function ($permission) use ($role) {
+            //         $role->givePermissionTo($permission);
+            //     });
+            // }
+            // if ($role->name === 'buyer') {
+            //     collect($buyer_permissions)->each(function ($permission) use ($role) {
+            //         $role->givePermissionTo($permission);
+            //     });
+            // }
             if ($role->name === 'warehouse manager') {
                 collect($warehouse_permissions)->each(function ($permission) use ($role) {
                     $role->givePermissionTo($permission);
                 });
             }
 
-            if ($role == 'financier') {
+            if ($role->name === 'financier') {
                 collect($transaction_permissions)->each(function($permission) use ($role) {
                     $role->givePermissionTo($permission);
                 });
             }
 
-            if ($role == 'inspector') {
+            if ($role->name === 'inspector') {
                 collect($inspector_permissions)->each(function($permission) use ($role) {
+                    $role->givePermissionTo($permission);
+                });
+            }
+
+            if ($role->name === 'deveint') {
+                collect($deveint_permissions)->each(function($permission) use ($role) {
                     $role->givePermissionTo($permission);
                 });
             }
