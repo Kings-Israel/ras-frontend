d284a2ec34ee8425e40cec96c06880491da4e60c
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-10-26 13:39:39 +0300

    Updates Roles and Permission

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -62,11 +63,19 @@ class UsersSeeder extends Seeder
         $user->assignRole('vendor');
 
         $user = User::factory()
-        ->create([
-            'email' => 'admin@deveint.com',
-            'phone_number' => '0700004337'
-        ]);
+                ->create([
+                    'email' => 'admin@deveint.com',
+                    'phone_number' => '0700004337'
+                ]);
 
         $user->assignRole('deveint');
+
+        $user = User::factory()
+                ->create([
+                    'email' => 'driver@ras.com',
+                    'phone_number' => '0700104337',
+                ]);
+
+        $user->assignRole('driver');
     }
 }
