9a2092b90f6fda62cb314aa59e50389d5476236d
steven-maina <stevenmaina17@gmail.com>
2023-10-30 13:55:33 +0300

    storage request and package

--- database/migrations/2023_10_26_231634_create_packagings_table.php
+++ database/migrations/2023_10_26_231634_create_packagings_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('store_requests', function (Blueprint $table) {
+        Schema::create('packagings', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('unit_of_measurement')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('store_requests');
+        Schema::dropIfExists('packagings');
     }
 };
